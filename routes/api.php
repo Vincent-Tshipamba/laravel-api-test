7dcaa247c5c86934cdecfd3f668b46a61efe3fb6
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-11-25 14:53:50 +0100

    Configuration et utilisation de Laravel Sanctum pour la securisation des requetes vers l'api

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Route::apiResource("users", UserController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('users', [UserController::class, 'store']);
+    Route::get('users/{user}', [UserController::class, 'show']);
+    Route::delete('users/{user}', [UserController::class, 'destroy']);
+    Route::post('users/{user}', [UserController::class, 'update']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
